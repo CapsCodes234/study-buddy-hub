b72c51d93eea276c3e1c7a71894c0c3b85bda81d
CapsCodes234 <acheampongsterling4@gmail.com>
2025-12-25 17:08:44 +0000

    fix: make Settings responsive

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -409,7 +409,7 @@ export const Settings = ({
             </div>
           </div>
 
-          <div className="grid grid-cols-2 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <div className="space-y-1.5">
               <Label>Main Exam Lead Days</Label>
               <Input
